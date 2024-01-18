f34b6b8320b1028d4d753f0ea960d770012d15a5
Max Davidse <m.davidse@rubicon.nl>
2024-01-18 09:28:27 +0100

    Added some environment and tweaked the boarder a little

--- Assets/_Project/Scripts/CrashDetector.cs
+++ Assets/_Project/Scripts/CrashDetector.cs
@@ -4,10 +4,13 @@ using UnityEngine.SceneManagement;
 public class CrashDetector : MonoBehaviour
 {
     [SerializeField] float _timeBeforeReload = 1f;
+    [SerializeField] ParticleSystem _crashEffect;
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Ground"))
         {
+            _crashEffect.Play();
             Invoke("LoseAndReloadLevel", _timeBeforeReload);
         }
     }
--- Assets/_Project/Scripts/FinishLine.cs
+++ Assets/_Project/Scripts/FinishLine.cs
@@ -4,10 +4,12 @@ using UnityEngine.SceneManagement;
 public class FinishLine : MonoBehaviour
 {
     [SerializeField] float _timeBeforeReload = 1f;
+    [SerializeField] ParticleSystem _finishEffect;
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
+            _finishEffect.Play();
             Invoke("FinishLevelAndReload", _timeBeforeReload);
         }
     }
